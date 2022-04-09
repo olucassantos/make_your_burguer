b71894919f4dceb622f38a351efd6fc99a8a8840
Lucas Santos <oargus.g@gmail.com>
2022-04-09 17:11:17 -0300

    Formulário Burger

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,21 @@
     <Banner />
 
     <div class="main-container">
-        <h1>HOME</h1>
+        <h1>Monte o seu burger:</h1>
+
+        <BurgerForm />
     </div>
 </template>
 
 <script>
 import Banner from '@/components/Banner.vue'
+import BurgerForm from '@/components/BurgerForm.vue'
 
 export default {
     name: 'Home',
     components: { 
-        Banner 
+        Banner,
+        BurgerForm 
     },
 
 }
