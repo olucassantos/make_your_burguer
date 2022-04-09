4b6b5d6800c72d344c03109cc51cb7c458444f7b
Lucas Santos <oargus.g@gmail.com>
2022-04-09 15:42:32 -0300

    Criação de Footer, Header  e CSS para as páginas

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-    <p>Footer</p>
+    <footer id="footer">
+        <p>Make Your Burguer &copy; 2021</p>
+    </footer>
 </template>
 
 <script>
@@ -8,6 +10,17 @@
     }
 </script>
 
-<style>
-
+<style scoped>
+    #footer{
+        height: 200px;
+        background-color: #222;
+        border-top: 4px solid #111;
+        color: #fcba03;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+    }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,20 +1,49 @@
 <template>
-    <nav>
-        <router-link to="/">
-            <img id="logo" src="" alt="">
+    <nav id="nav">
+        <router-link to="/" id="logo-url">
+            <img id="logo" :src="logo" :alt="alt">
         </router-link>
 
-        <router-link to="/">Home</router-link> |
-        <router-link to="/pedidos">About</router-link>
+        <router-link to="/">Home</router-link>
+        <router-link to="/pedidos">Pedidos</router-link>
     </nav>
 </template>
 
 <script>
     export default {
         name: "Navbar",
+        props: ["logo", "alt"]
     }
 </script>
 
 <style scoped>
+    #nav{
+        background-color: #222;
+        border-bottom: 4px solid #111;
+        padding: 15px 50px;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+    }
 
+    #nav #logo-url{
+        margin: auto;
+        margin-left: 0;
+    }
+
+    #logo{
+        width: 40px;
+        height: 40px;
+    }
+
+    #nav a{
+        color: #fcba03;
+        text-decoration: none;
+        margin: 12px;
+        transition: .5s;
+    }
+
+    #nav a:hover{
+        color: #fff;
+    }
 </style>
