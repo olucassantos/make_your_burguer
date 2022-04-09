5ac79060713887a3813ce399a0efffc9c08d7885
Lucas Santos <oargus.g@gmail.com>
2022-04-09 14:08:08 -0300

    Estrutura de Header e Footer Alteração das rotas Criação de rotas

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav>
+        <router-link to="/">
+            <img id="logo" src="" alt="">
+        </router-link>
+
+        <router-link to="/">Home</router-link> |
+        <router-link to="/pedidos">About</router-link>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "Navbar",
+    }
+</script>
+
+<style scoped>
+
+</style>
