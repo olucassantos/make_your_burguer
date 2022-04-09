619470f41198151f4bc2fc2bc3f860f06af344f9
Lucas Santos <oargus.g@gmail.com>
2022-04-09 16:49:27 -0300

    Banner da home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <template>
+    <Banner />
+
     <div class="main-container">
         <h1>HOME</h1>
     </div>
 </template>
 
 <script>
+import Banner from '@/components/Banner.vue'
 
+export default {
+    name: 'Home',
+    components: { 
+        Banner 
+    },
+
+}
 </script>
