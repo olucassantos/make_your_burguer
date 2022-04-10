408669a671b9c33e9f95a02b564a79b4c5996e84
Lucas Santos <oargus.g@gmail.com>
2022-04-10 02:30:03 -0300

    Buscando pedidos do banco Buscando status do banco

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,130 @@
+<template>
+    <div id="burger-table">
+        <div>
+            <div id="burger-table-heading">
+                <div class="order-id">#:</div>
+                <div>Cliente:</div>
+                <div>Pão:</div>
+                <div>Carne:</div>
+                <div>Opcionais:</div>
+                <div>Ações:</div>
+            </div>
+            
+            <div id="burger-table-rows">
+                <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+                    <div class="order-number">{{ burger.id }}</div>
+                    <div>{{ burger.nome }}</div>
+                    <div>{{ burger.pao }}</div>
+                    <div>{{ burger.carne }}</div>
+                    <div>
+                        <ul>
+                            <li v-for="(opcional, index) in burger.opcionais" :key="index">
+                                {{ opcional }}
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <select class="status" name="status">
+                            <option value="">Selecione</option>
+                            <option v-for="stat in status" :key="stat.id" value="stat.tipo" :selected="burger.status == stat.tipo">
+                                {{ stat.tipo }}
+                            </option>
+                        </select>
+
+                        <button class="delete-btn">Cancelar</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Dashboard",
+    data (){
+        return {
+            burgers: null,
+            burger_id: null,
+            status: []
+        }
+    },
+    mounted (){
+        this.getPedidos();
+        this.getStatus();
+    },
+    methods:{
+        async getPedidos() {
+            const requisicao = await fetch('http://localhost:3000/burgers');
+            const data = await requisicao.json();
+
+            this.burgers = data;
+        },
+        async getStatus() {
+            const requisicao = await fetch('http://localhost:3000/status');
+            const data = await requisicao.json();
+
+            this.status = data;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    #burger-table{
+        max-width: 1200px;
+        margin: 0 auto;
+    }
+
+    #burger-table-heading,
+    #burger-table-rows,
+    .burger-table-row{
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    #burger-table-heading{
+        font-weight: bold;
+        padding: 12px;
+        border-bottom: 3px solid #333;
+    }
+
+    #burger-table-heading div,
+    .burger-table-row div{
+        width: 19%;
+    }
+
+    .burger-table-row{
+        width: 100%;
+        padding: 12px;
+        border-bottom: 1px solid #ccc;
+    }
+
+    #burger-table-heading .order-id,
+    .burger-table-row .order-number{
+        width: 5%;
+    }
+
+    select{
+        padding: 12px 6px;
+        margin-right: 12px;
+    }
+
+    .delete-btn{
+        background-color: #222;
+        color: #fcba03;
+        border: 2px solid #222;
+        padding: 10px;
+        font-size: 16px;
+        margin: 0 auto;
+        cursor: pointer;
+        transition: .5s;
+    }
+
+    .delete-btn:hover{
+        background-color: transparent;
+        color: #222;
+    }
+
+</style>
